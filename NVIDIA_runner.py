c55ee83ab005d84abd5a4cae8a6507f392849eb9
gitaumark <156354973+gitaumark@users.noreply.github.com>
2025-03-04 18:07:56 -0500

    Update NVIDIA_runner.py

--- NVIDIA_runner.py
+++ NVIDIA_runner.py
@@ -2,7 +2,6 @@ import os
 import sys
 import subprocess
 from Benchmarks.NVIDIA import GEMMCublasLt as gemm
-from Benchmarks.NVIDIA import HBMBandwidth as HBM
 from Benchmarks.NVIDIA import NVBandwidth as NV
 from Benchmarks.NVIDIA import NCCLBandwidth as NCCL
 from Benchmarks.NVIDIA import FlashAttention as FA
@@ -19,12 +18,7 @@ def run_CublasLt():
     test = gemm.GEMMCublastLt("config.json",machine_name) 
     test.build()
     test.run_model_sizes()
-    
-def run_HBMBandwidth():
-    results = subprocess.run("./Benchmarks/NVIDIA/stream_vectorized_double_test -n1073741824 -r1", shell=True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-    print(results.stdout.decode('utf-8'))
-    print(results.stderr.decode('utf-8'))
-    
+       
 def run_NVBandwidth():
     test = NV.NVBandwidth("config.json", machine_name)
     test.build()
@@ -65,12 +59,7 @@ if ("nccl" in arguments):
     match = True
     run_NCCLBandwidth()
     os.chdir(current)
-    
-if ("hbm" in arguments):
-    match = True
-    run_HBMBandwidth()
-    os.chdir(current)
-    
+       
 if ("nv" in arguments):
     match = True
     run_NVBandwidth()
@@ -99,13 +88,11 @@ if ("all" in arguments):
     run_Multichase()
     run_CPUStream()
     os.chdir(current)
-    run_HBMBandwidth()
-    os.chdir(current)
     run_NVBandwidth()
     os.chdir(current)
     run_FlashAttention()
     os.chdir(current)
 
 if not match: 
-    print("Usage: python3 NVIDIA_runner.py [arg]\n   or: python3 NVIDIA_runner.py [arg1] [arg2] ... to run more than one test e.g python3 NVIDIA_runner.py hbm nccl\nArguments are as follows, and are case insensitive:\nAll tests:  all\nCuBLASLt GEMM:  gemm\nNCCL Bandwidth: nccl\nHBMBandwidth:   hbm\nNV Bandwidth:   nv\nFlash Attention: fa\nCPU Stream: cpustream\nMultichase:  multichase")
+    print("Usage: python3 NVIDIA_runner.py [arg]\n   or: python3 NVIDIA_runner.py [arg1] [arg2] ... to run more than one test e.g python3 NVIDIA_runner.py gemm nccl\nArguments are as follows, and are case insensitive:\nAll tests:  all\nCuBLASLt GEMM:  gemm\nNCCL Bandwidth: nccl\nNV Bandwidth:   nv\nFlash Attention: fa\nCPU Stream: cpustream\nMultichase:  multichase")
     
